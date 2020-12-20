ef4de0398caeaee829e530c8878e1f158dd8822f
justinfargnoli <justinfargnoli@gmail.com>
2020-12-20 15:06:54 -0500

    Download patient 1174683's data.

--- bin/learn_fhir.dart
+++ bin/learn_fhir.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:fhir_at_rest/fhir_at_rest.dart';
+import 'package:fhir/r4.dart';
+
+void main(List<String> arguments) async {
+  final req = ReadRequest.r4(
+    base: Uri.parse('https://hapi.fhir.org/baseR4'),
+    type: R4Types.patient,
+    id: Id('1174683'),
+  );
+  final makeReq = await req.request();
+  print(makeReq);
 }
